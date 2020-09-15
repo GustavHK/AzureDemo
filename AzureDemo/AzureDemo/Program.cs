456fbf1f40629a13d8c9afcdd940feb5377c5dc8
Gustav <gust1224@live.dk>
2020-09-15 17:47:33 +0200

    Fixed

--- AzureDemo/AzureDemo/Program.cs
+++ AzureDemo/AzureDemo/Program.cs
@@ -6,7 +6,7 @@ namespace AzureDemo
     {
         static void Main(string[] args)
         {
-            Consolegg.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
